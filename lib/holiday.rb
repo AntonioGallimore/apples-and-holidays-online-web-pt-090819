9882b1fd47dde093cf4fdf00b5cf025a25568fa5
Antonio Gallimore <antonio.gallimore@gmail.com>
2019-09-24 17:03:17 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,7 +27,9 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
    holiday_hash.each do |season, holiday|
      if season == :winter
-   binding.pry
+      holiday.each do |day, supplies|
+        binding.pry
+    end
   end
  end
 end
