5e2c5a98d90f5bbfb8cac76b7ad818ad437b144e
Antonio Gallimore <antonio.gallimore@gmail.com>
2019-09-24 17:43:54 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,6 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
    holiday_hash.each do |season , holiday|
      puts "#{season.to_s.capitalize}:"
      holiday.each do |day, supplies|
+     end
  end
 end
 def all_holidays_with_bbq(holiday_hash)
